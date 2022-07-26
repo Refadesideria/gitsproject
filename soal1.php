ec1eef0003d0b5e74b0542e36f5332aa05295855
Refadesideria <refadesi01@gmail.com>
2022-07-26 15:17:35 +0700

    Project Test Gits

---
+++ soal1.php
@@ -0,0 +1,23 @@
+<?php
+
+// array bagian A
+$no = [1,1,0,1,1,1];
+
+echo "Soal 1 Bagian A";
+echo"<br>";
+
+echo $no[0] - $no[1] + $no[3] + $no[4] + $no[5];
+ 
+echo"<br>";
+echo"<br>";
+
+echo "Soal 1 Bagian B";
+echo"<br>";
+
+// array bagian B
+$nomor=[1,0,0,1,0,1,1];
+
+echo $nomor[0] - $nomor[1] + $nomor[3] + $nomor[4] + $nomor[5] - $nomor[6];
+
+?>
+
---
+++ soal2.php
@@ -0,0 +1,17 @@
+<?php
+
+$hello = [
+    "H",
+    "E-",
+    "L-",
+    "L-",
+    "O-",
+  ];
+
+  echo $hello[4];
+  echo $hello[3];
+  echo $hello[2];
+  echo $hello[1];
+  echo $hello[0];
+
+  ?>
